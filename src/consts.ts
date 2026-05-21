d6ec6c26249da6611a30bbc6b79af6006f77c378
Shan <shahnurislam@hotmail.co.uk>
2026-05-21 23:57:51 +0100

    Site: snippets syntax highlighting + styling; add CV page; update header nav

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Shan Islam';
+export const SITE_DESCRIPTION = 'Pricing, experimentation, forecasting and practical data science.';
