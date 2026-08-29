458685ab00e1ae1a127b4383f0c8d46d26f86529
Shan <shahnurislam@hotmail.co.uk>
2026-08-29 12:19:07 +0100

    Add Selected Work portfolio

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Shan Islam';
-export const SITE_DESCRIPTION = 'Pricing, experimentation, forecasting and practical data science.';
+export const SITE_DESCRIPTION = 'Lead Data Scientist specialising in pricing, forecasting, experimentation and applied machine learning.';
